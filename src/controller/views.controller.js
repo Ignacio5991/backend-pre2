2fa0ac852767db5bd45f13c172860fcacac3b9a3
Ignacio Gonzalez Sorribas <igonzalezsorribas@gmail.com>
2023-02-17 14:51:31 -0300

    Solucionando el paginate

--- src/controller/views.controller.js
+++ src/controller/views.controller.js
@@ -3,9 +3,10 @@ const BdCartManager = require ('../dao/mongoManager/BdCartManager');
 
 
 const views = async (req, res) => {
-  const products = await ProductManager.getProduct();
+  const page = req.query.page
+  const products = await ProductManager.getProduct(page);
   const view = products.docs.map((products) => ({ title: products.title, description: products.description, price: products.price, stock: products.stock, thumbnail: products.thumbnail}));
-  res.render('home', { products: view, hasPrevPage: !products.hasPrevPage, hasNextPage: !products.hasNextPage, page: !products.page });
+  res.render('home', { products: view, hasPrevPage: products.hasPrevPage, hasNextPage: products.hasNextPage, page: products.page, totalPages:products.totalPages });
 }
 
 
